1b8b7b7a46672b5c87eec7adc7b2775df13ac669
Ingrid <A00826973@itesm.mx>
2022-09-10 16:49:43 -0500

    accuracy

--- RegLog_Framework.py
+++ RegLog_Framework.py
@@ -11,20 +11,21 @@ df.columns = ["sepal length", "sepal width", "petal length", "petal width","spec
 #Calcular correlación
 corr = df.corr()
 
+#Dividir el dataset
+x = df.drop(["species"], axis=1)
+y = df['species']
+xtrain, xtest, ytrain, ytest = train_test_split(x,y,test_size=0.15, random_state=42)
+
 #transformación de la variable species en valores numéricos
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 df["species"] = le.fit_transform(df["species"])
 ytest = le.transform(ytest)
 
-#Dividir el dataset
-x = df.drop(["species"], axis=1)
-y = df['species']
-xtrain, xtest, ytrain, ytest = train_test_split(x,y,test_size=0.15, random_state=42)
-
 #Regresión logística
 from sklearn.linear_model import LinearRegression
 import sklearn.metrics
+from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 
@@ -34,9 +35,11 @@ ypred = lrm.predict(xtest)
 print("Coeficients and Interception: ",lrm.coef_, lrm.intercept_)
 
 #Métricas
+accuracy = accuracy_score(ytest, ypred.round())
 r2 = r2_score(ytest, ypred)
 mse = mean_squared_error(ytest, ypred)
 rmse = np.sqrt(mse)
+print("Accuracy: ", accuracy)
 print("R2 score: ", r2)
 print ("Mse: ", mse)
 print("Rmse: ", rmse)
